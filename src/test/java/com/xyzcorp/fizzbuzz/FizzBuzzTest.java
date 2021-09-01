80a62f7f19745121c48ab0ba84c4f8f36761fa89
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-09-01 11:17:55 -0600

    Reset FizzBuzzTest

--- src/test/java/com/xyzcorp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/xyzcorp/fizzbuzz/FizzBuzzTest.java
@@ -1,54 +1,5 @@
 package com.xyzcorp.fizzbuzz;
 
-import org.assertj.core.api.ThrowableAssert;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 public class FizzBuzzTest {
 
-    @Test
-    void testOne() {
-        //Think about the design
-        String result = FizzBuzz.apply(1);
-        assertThat(result).isEqualTo("1");
-    }
-
-    @Test
-    void testTwo() {
-        String result = FizzBuzz.apply(2);
-        assertThat(result).isEqualTo("2");
-    }
-
-    @Test
-    void testThree() {
-        String result = FizzBuzz.apply(3);
-        assertThat(result).isEqualTo("Fizz");
-    }
-
-    @Test
-    void testFive() {
-        String result = FizzBuzz.apply(5);
-        assertThat(result).isEqualTo("Buzz");
-    }
-
-    @Test
-    void testFifteen() {
-        String result = FizzBuzz.apply(15);
-        assertThat(result).isEqualTo("FizzBuzz");
-    }
-
-    @Test
-    void testThirty() {
-        String result = FizzBuzz.apply(30);
-        assertThat(result).isEqualTo("FizzBuzz");
-    }
-
-    @Test
-    void testNegativeNumber() {
-        assertThatThrownBy(() -> FizzBuzz.apply(-1))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("FizzBuzz can only be applied to positive numbers");
-    }
 }
