3fc9c30e4eaa0a7d819bd7371fe533c37dec42a2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-12-13 14:44:15 -0500

    Added Thread switching to ReactiveTest.java

--- src/test/java/com/xyzcorp/reactive/ReactiveTest.java
+++ src/test/java/com/xyzcorp/reactive/ReactiveTest.java
@@ -1,6 +1,7 @@
 package com.xyzcorp.reactive;
 
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.jupiter.api.Test;
 
 public class ReactiveTest {
@@ -26,5 +27,19 @@ public class ReactiveTest {
             .subscribe(System.out::println,
                 Throwable::printStackTrace,
                 () -> System.out.println("Done"));
+    }
+
+    @Test
+    void testReactiveWithThreadBoundariesOrBulkheads() {
+        Observable.just(1,2,3,4,5)
+            .observeOn(Schedulers.computation())
+            .subscribeOn(Schedulers.io())
+            .map(i -> i * 10)
+            .observeOn(Schedulers.io())
+            .subscribe(this::updateTheDatabase);
+    }
+
+    private void updateTheDatabase(Integer i) {
+        //dummy
     }
 }
